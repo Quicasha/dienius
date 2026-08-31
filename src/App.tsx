0a1ddabd5c352aa693d0cf8c327f9fc4f600909c
Quicasha <dovjys@gmail.com>
2026-08-31 11:57:25 +0300

    chore: scaffold Vite React TypeScript app with Vitest

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <span className="brand">Dienius</span>
+      </header>
+    </div>
+  )
+}
